9b4c7a5fc5ae5bf0e3d238d0fd4aa17eaea216b0
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-02-27 20:54:08 +0530

    Add files via upload

---
+++ input .java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+ 
+class Input
+{
+    public static void main(String[] args){
+     int num;
+     System.out.println("Enter the number:");
+     Scanner scanner=new Scanner(System.in);
+     num = scanner.nextInt();
+          System.out.println("Entered number:"+num);
+
+}
+}
+
