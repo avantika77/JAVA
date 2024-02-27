6749a2ca6053b2f6c5b13e140b6e2b9eac17c46c
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-02-27 21:57:49 +0530

    Update input.java

--- input.java
+++ input.java
@@ -5,11 +5,14 @@
 ScannerObject is the beforehand created object of the Scanner class
 ===============================================================================================
 import java.io.*; 
-import java.util.Scanner;
- 
+import java.util.Scanner; //This line imports the Scanner class from the java.util package.
+//The Scanner class is used to obtain user input from the console.
 class Input
 {
-    public static void main(String[] args){
+    public static void main(String[] args)//his is the entry point of the Java program.
+ //It is a special method that JVM (Java Virtual Machine) looks for to start the execution of the program.
+ //It takes an array of strings (args) as input parameters.
+ {
      int num;
      System.out.println("Enter the number:");
      Scanner scanner=new Scanner(System.in);
