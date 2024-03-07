a16d2c99ca280ca1a196a904acc42efd1ee6e913
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-03-07 23:07:06 +0530

    Add files via upload

---
+++ deafult value of datatype.java
@@ -0,0 +1,29 @@
+import java.io.*; 
+class defult{
+    static byte b;
+    static int i;
+    static long l;
+    static double d;
+    static short s;
+    static boolean bool;
+    static char c;
+    static String str;
+    static Object object;
+    static float f;
+    static int[] array;
+    public static void main(String[] args)
+    {
+        System.out.println("byte value " + b);
+        System.out.println("short value " + s);
+        System.out.println("int value " + i);
+        System.out.println("long value " + l);
+        System.out.println("double value " + d);
+        System.out.println("boolean value " + bool);
+        System.out.println("char  value " + c);
+        System.out.println("String value " + str);
+        System.out.println("array  value " + object);
+        System.out.println("byte value " + array);
+        
+    }
+
+} 
