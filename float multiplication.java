400c947c696958b6352c62c90b7a8428f236f674
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-03-10 07:39:08 +0530

    Update float multiplication.java

--- float multiplication.java
+++ float multiplication.java
@@ -1,1 +1,16 @@
+// Java Program to print Multiplication of two floating
 
+import java.io.*;
+class Program {
+	public static void main(String[] args)
+	{
+		float f1 = 1.5f;
+		float f2 = 2.0f;
+		float f3 = f1 * f2;
+		System.out.println("The product is: " + f3);
+	}
+}
+
+=================================================================================================================
+  OUTPUT:The product is: 3.0
+  ===============================================================================================================
