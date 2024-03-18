a3041fc78f4878b7101c7b4fc88cbf35c5635b94
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-03-18 20:57:58 +0530

    Update Simple interest.java

--- Simple interest.java
+++ Simple interest.java
@@ -25,3 +25,22 @@ Enter the value of principal : 45
 Enter the value of rate : 3 
 Enter the value of time : 7
 Simple Interest: 9.45
+=============================================================================================
+import java.util.Scanner;
+class CompoundInterest {
+    public static void main(String args[]) {
+        float p, r, t;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of principal: ");
+        p = scanner.nextFloat();
+        System.out.print("Enter the value of rate: ");
+        r = scanner.nextFloat();
+        System.out.print("Enter the value of time: ");
+        t = scanner.nextFloat();
+        
+        double compound = p * (Math.pow(1 + r / 100, t));
+        System.out.println("Compound Interest: " + compound);
+        scanner.close();
+    }
+}
