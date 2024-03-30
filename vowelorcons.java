b5dc85aea08b660ea1dbb60c0cff6d6c94ef84ba
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-03-30 17:05:17 +0530

    Update and rename vowelorcons. to vowelorcons.java

--- vowelorcons.java
+++ vowelorcons.java
@@ -1,6 +1,7 @@
-import java.io.*;   
+import java.io.*;        // imports the java.io package, which contains classes for input and output operations.
 public class CHECK { 
-    static void VoOrCont(char y) 
+    static void VoOrCont(char y) // static method named VoOrCont, which takes a single parameter of type char named y.
+    //The method does not return any value (void). This method is used to check whether the given character is a vowel or a consonant.
     { 
         if (y == 'a' || y == 'e' || y == 'i' || y == 'o'                           //uppercase and lowercase
             || y == 'u'|| y == 'A' || y == 'E' || y == 'I'
@@ -9,7 +10,8 @@ public class CHECK {
         else
             System.out.println("It is a Consonant."); 
     } 
-    static public void main(String[] args) 
+    static public void main(String[] args) // the main method, which is the entry point of the program. 
+    //It takes an array of String objects as its parameter.
     { 
         VoOrCont('b'); 
         VoOrCont('u'); 
