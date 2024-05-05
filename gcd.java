be2b961a43a915569b36e7bebe3a4d839802bc70
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-05-05 16:45:57 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -2,25 +2,26 @@
 //GCD: greatest commmon factor or hcf ( Highest Common Factor)is the largest number that can be divide both the given number
 //60=2x2x3x5            [GCD:2x2x23=12]
 //36=2x2x3x3
-// class check
-// {
-//     static int check(int x, int y) // arguments
-//     {
-//      if(x==0)
-//      return y;
-//      if(y==0)
-//      return x;
-//      if (x>y)
-//      return check (x-y,y);
-//      return check(x,y-x);
-//     }
-//     public static void main(String[] args)
-//     {
-//         int x= 100 ,y=90;
-//         System.out.println("GCD of " + x + " and " + y 
-//         + " is " + check(x, y));
-//     }
-// }
+class check
+{
+    static int check(int x, int y) // arguments
+    {
+     if(x==0)
+     return y;
+     if(y==0)
+     return x;
+     if (x>y)
+     return check (x-y,y);
+     return check(x,y-x);
+    }
+    public static void main(String[] args)
+    {
+        int x= 100 ,y=90;
+        System.out.println("GCD of " + x + " and " + y 
+        + " is " + check(x, y));
+    }
+}
+
 //An efficient solution is to utilize a modulo operator in the Euclidean algorithm which is the foremost algorithm applied for this topic.
 class check{
     static int check(int x, int y)
